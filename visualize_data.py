2d86c7d4d198867562d5ff168d9ed5198773ff8c
andrewtan1018 <tan434@purdue.edu>
2023-03-20 15:48:12 -0400

    Condensed the visualize_data.py file. It now plots data without having to separate data into PEARLS and BANANAS

--- visualize_data.py
+++ visualize_data.py
@@ -7,35 +7,47 @@ symbol = 'BANANAS'
 data_day0 = pd.read_csv("Data/prices_round_1_day_0.csv")
 data_day_1 = pd.read_csv("Data/prices_round_1_day_-1.csv")
 data_day_2 = pd.read_csv("Data/prices_round_1_day_-2.csv")
+data_day_1['timestamp'] = data_day_1['timestamp'] + 1000000
+data_day0['timestamp'] = data_day0['timestamp'] + 2 * 1000000
+data_unfiltered = pd.concat([data_day_2, data_day_1, data_day0])
+data_unfiltered.to_csv("Data/all_3_days.csv")
 
+## Getting the data for the specified symbol by finding each day's data, then concatenating
+# x0 = data_day0.drop(data_day0.loc[data_day0['product'] != symbol].index)['timestamp']
+# y0 = data_day0.drop(data_day0.loc[data_day0['product'] != symbol].index)['mid_price']
+# x_1 = data_day_1.drop(data_day_1.loc[data_day_1['product'] != symbol].index)['timestamp']
+# y_1 = data_day_1.drop(data_day_1.loc[data_day_1['product'] != symbol].index)['mid_price']
+# x_2 = data_day_2.drop(data_day_2.loc[data_day_2['product'] != symbol].index)['timestamp']
+# y_2 = data_day_2.drop(data_day_2.loc[data_day_2['product'] != symbol].index)['mid_price']
+#
+# x = pd.concat([x_2, x_1, x0])
+# y = pd.concat([y_2, y_1, y0])
 
-data_day0.drop(data_day0.loc[data_day0['product'] != symbol].index, inplace=True)
-data_day_1.drop(data_day_1.loc[data_day_1['product'] != symbol].index, inplace=True)
-data_day_2.drop(data_day_2.loc[data_day_2['product'] != symbol].index, inplace=True)
-print(data_day0.head())
+## Outputting data specific to each symbol
+# data = pd.concat([data_day_2, data_day_1, data_day0])
+# data['timestamp'] = x
+# print(data.tail())
+# data.to_csv("Data/" + symbol + "_3_days.csv")
 
-# Day -2 is the start of the data
-x_2 = data_day_2['timestamp']
-y_2 = data_day_2['mid_price']
+x_pearls = data_unfiltered.drop(data_unfiltered.loc[data_unfiltered['product'] == 'PEARLS'].index)['timestamp']
+y_pearls = data_unfiltered.drop(data_unfiltered.loc[data_unfiltered['product'] == 'PEARLS'].index)['mid_price']
+x_bananas = data_unfiltered.drop(data_unfiltered.loc[data_unfiltered['product'] == 'BANANAS'].index)['timestamp']
+y_bananas = data_unfiltered.drop(data_unfiltered.loc[data_unfiltered['product'] == 'BANANAS'].index)['mid_price']
 
-# Recording data for day -1 // 1 day after beginning
-x_1 = data_day_1['timestamp'] + 1000000  # Each new day increments the timestamp by 100
-y_1 = data_day_1['mid_price']
+## Plotting data for PEARLS and BANANAS side by side
+# figure, axis = plt.subplots(1, 2)
+# axis[0].plot(x_pearls, y_pearls)
+# axis[0].set_xlabel("Time stamp")
+# axis[0].set_ylabel("Mid_Price")
+# axis[0].set_title("Market for PEARLS")
+#
+# axis[1].plot(x_bananas, y_bananas)
+# axis[1].set_xlabel("Time stamp")
+# axis[1].set_ylabel("Mid_Price")
+# axis[1].set_title("Market for BANANAS")
 
-# Recording data for day 0 // 2 days after beginning
-x0 = data_day0['timestamp'] + 2 * 1000000
-y0 = data_day0['mid_price']
-
-x = pd.concat([x_2, x_1, x0])
-y = pd.concat([y_2, y_1, y0])
-
-data = pd.concat([data_day_2, data_day_1, data_day0])
-data['timestamp'] = x
-print(data.tail())
-data.to_csv("Data/" + symbol + "_3_days.csv")
-
-plt.plot(x, y)
+plt.plot(x_pearls, y_pearls)
 plt.xlabel("Time stamp")
 plt.ylabel("Mid_Price")
-plt.title("Market for " + symbol)
+plt.title("Market for pearls")
 plt.show()
